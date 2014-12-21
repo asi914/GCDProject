3c0039db8b90cc9fdaaa9044aba50f2324a357c1
Ayush <asi914@users.noreply.github.com>
2014-12-21 06:37:19 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,9 @@ run_analysis <- function() {
   ## 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
   ## 3) Uses descriptive activity names to name the activities in the data set
   ## 4) Appropriately labels the data set with descriptive variable names. 
-  ## 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.  
+  ## 5) From the data set in step 4, creates a second, independent tidy data set with the average of 
+  ## each variable for each activity and each subject
+  ## Writes the final tidy dataset to file and returns the dataframe 
   
   
   # 1) process the test set data
